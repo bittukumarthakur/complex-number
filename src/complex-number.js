253a95a0c8bd89f6a19cad96be6bd85e4fc5d6bb
Bittu <bittukumarthakur@users.noreply.github.com>
2023-05-03 10:20:39 +0530

    Implemented a function which multiply complex number.

--- src/complex-number.js
+++ src/complex-number.js
@@ -1,26 +1,71 @@
-// getReal({real: 2, img: 3}) ==> 3;
-// getReal([2,3i]) ==> 3;
+// getReal({real: 2, imaginary: 3}) ==> 3;
+
+const get = function(complexNumber) {
+  const real = function() {
+    return complexNumber.real;
+  };
+
+  const imaginary = function() {
+    return complexNumber.imaginary;
+  };
+
+  return {real,imaginary}
+};
+
 
 const getReal = function(complexNumber) {
   return complexNumber.real;
 };
 
-const getImg = function(complexNumber) {
-  return complexNumber.img;
+const getImaginary = function(complexNumber) {
+  return complexNumber.imaginary;
 };
 
+
+const add = function(firstNumber, secondNumber) {
+  const a = get(firstNumber);
+  const b = get(secondNumber);
+  const real = a.real() + b.real();
+  const imaginary = a.imaginary() + b.imaginary();
+
+  return {real, imaginary};
+};
+
+/*
 const add = function(firstNumber, secondNumber) {
   const real = firstNumber.real + secondNumber.real;
-  const img = firstNumber.img + secondNumber.img;
-  return {real, img};
+  const imaginary = firstNumber.imaginary + firstNumber.imaginary;
+
+  return {real, imaginary};
 }
+ */
+
+// complex multiplication
+// (a + bi) * (c + di)
+//a(c + di) + bi(c + di)
+// (a * c) + (a * di) + (bi * c) + (bi * di)
+// (real1 * real2) + (real1 * imaginary2) + (imaginary1 * real2) + (imaginary1 * imaginary2 * -1)
+// real: (real1 * real2) + (imaginary1 * imaginary2 -1)
+// imaginary: (real1 * imaginary2) + (imaginary1 * real2);
+
+const multiply = function(firstNumber, secondNumber) {
+  const {real: real1, imaginary: imaginary1} = firstNumber;
+  const {real: real2, imaginary: imaginary2} = secondNumber;
+  const real = (real1 * real2) + (imaginary1 * imaginary2 * -1);
+  const imaginary = (real1 * imaginary2) + (imaginary1 * real2);
+
+  return {real, imaginary};
+};
 
 const display = function(complexNumber) {
-  console.log(`${complexNumber.real} + ${complexNumber.img}i`);
+  const {real, imaginary} = complexNumber;
+  const operator = imaginary < 0 ? "-" : "+";
+  console.log(`${real} ${operator} ${Math.abs(imaginary)}i`);
 };
 
 exports.getReal = getReal;
-exports.getImg = getImg;
+exports.getImaginary = getImaginary;
 exports.add = add;
 exports.display = display;
+exports.multiply = multiply;
 
--- test/complex-number-test.js
+++ test/complex-number-test.js
@@ -1,43 +1,49 @@
 const {it, describe} = require("node:test");
 const {strictEqual, deepStrictEqual } = require("assert");
-const {getReal, getImg, add, display} = require("../src/complex-number.js");
-
-console.log(getReal);
+const {getReal, getImaginary, add, display} = require("../src/complex-number.js");
 
 describe("get the real part", function() {  
   it("should return 2", function() {
-    strictEqual(getReal({real: 2, img: 3}), 2);
+    strictEqual(getReal({real: 2, imaginary: 3}), 2);
   });
 
   it("should return 4", function() {
-    strictEqual(getReal({real: 4, img: 1}), 4);
+    strictEqual(getReal({real: 4, imaginary: 1}), 4);
   });
 
   it("should return 20", function() {
-    strictEqual(getReal({real: 20, img: 4}), 20);
+    strictEqual(getReal({real: 20, imaginary: 4}), 20);
   });
 });
 
 describe("get the imiginary part", function() {  
   it("should return 3", function() {
-    strictEqual(getImg({real: 2, img: 3}), 3);
+    strictEqual(getImaginary({real: 2, imaginary: 3}), 3);
   });
 
   it("should return 1", function() {
-    strictEqual(getImg({real: 4, img: 1}), 1);
+    strictEqual(getImaginary({real: 4, imaginary: 1}), 1);
   });
 
   it("should return 10", function() {
-    strictEqual(getImg({real: 20, img: 10}), 10);
+    strictEqual(getImaginary({real: 20, imaginary: 10}), 10);
   });
 });
 
 describe("sum of complex number.", function() {  
-  it("should return 5 + 5i", function() {
-    deepStrictEqual(add({real: 2, img: 1}, {real: 3, img: 4}), {real: 5, img: 5});
+  it("should contain all +ve number.", function() {
+    deepStrictEqual(add({real: 2, imaginary: 1}, {real: 3, imaginary: 4}), {real: 5, imaginary: 5});
+  });
+
+  it("should contain all -ve number.", function() {
+    deepStrictEqual(add({real:-2 , imaginary: -3}, {real: -3, imaginary: -4}), {real: -5, imaginary: -7});
+  });
+
+  it("should contain both +ve and -ve number.", function() {
+    deepStrictEqual(add({real:2 , imaginary: -3}, {real: -3, imaginary: 4}), {real: -1, imaginary: 1});
   });
 
 });
 
-display({real: 1,img: 2})
+display({real: 1,imaginary: 2})
 
